9c0302a3d7499ff4583d99b12c1439ce3c929fda
Hazel Wu <tinazx056@gmail.com>
2022-04-12 00:37:00 +0800

    feat(app): get Rooms

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-export default (req, res) => {
-  console.log(req)
+import Room from './models/room.js'
+const headers = {
+  'Content-Type': 'application/json',
+  'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+  'Aceess-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET POST OPTIONS DELETE PATCH'
+}
+
+export default async (req, res) => {
+  if (req.method == 'GET' && req.url == '/rooms') {
+    const room = await Room.find()
+    res.writeHead(200, headers)
+    res.write(JSON.stringify({
+      status: 'success',
+      data: room
+    }))
+    res.end()
+  }
 }
