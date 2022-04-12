03a898dc78a61b23f8a45b65960cc4413951dc1e
Hazel Wu <tinazx056@gmail.com>
2022-04-12 23:49:05 +0800

    feat: install dotenv, connect remote db

--- db.js
+++ db.js
@@ -1,7 +1,13 @@
 import mongoose from 'mongoose'
+import 'dotenv/config'
+
+const DB = process.env.DB_URL.replace(
+  '<PASSWORD>',
+  process.env.DB_PASSWORD
+)
 
 async function main() {
-  await mongoose.connect('mongodb://localhost:27017/test')
+  await mongoose.connect(DB)
 }
 
 export default main
